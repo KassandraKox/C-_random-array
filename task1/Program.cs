0b73d876196d807ac9cd40e80c5d7ec46ea8d14b
Anastasia <sawiii@mail.ru>
2022-08-30 16:41:24 +0300

    First task

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,15 @@
 ﻿// 5зн число, является ли палиндромом.
 // Число хранить в int, решать с помощью арифм операций (не string)
-int[] array = new int [5] {1, 4, 2, 1, 2}; // массив из 5 чисел
-Console.WriteLine(array);
-//Console.WriteLine(array[0]);
+int[] array = new int [5] {1, 2, 2, 2, 1}; // массив из 5 чисел
+Console.WriteLine(string.Join(",", array));
+if (array[0] == array[4])
+{
+    if (array[1] == array[3])
+    {
+        Console.WriteLine("Палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
