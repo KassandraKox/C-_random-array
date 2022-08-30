65a522a0e73ef708f5304f4d42b6e15c26877aff
Anastasia <sawiii@mail.ru>
2022-08-30 11:16:37 +0300

    1task

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿// 5зн число, является ли палиндромом.
+// Число хранить в int, решать с помощью арифм операций (не string)
+int[] array = new int [5] {1, 4, 2, 1, 2}; // массив из 5 чисел
+Console.WriteLine(array);
+//Console.WriteLine(array[0]);
