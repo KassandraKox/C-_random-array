fb0f4786aa15acb3da40e72f9d3b95eced52d8a6
Anastasia <sawiii@mail.ru>
2022-08-31 10:33:10 +0300

    Changed task1 with massive

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,12 +1,16 @@
 ﻿// 5зн число, является ли палиндромом.
 // Число хранить в int, решать с помощью арифм операций (не string)
-int[] array = new int [5] {1, 2, 2, 2, 1}; // массив из 5 чисел
+int[] array = new int [5] {2, 2, 3, 2, 2}; // массив из 5 чисел
 Console.WriteLine(string.Join(",", array));
 if (array[0] == array[4])
 {
     if (array[1] == array[3])
     {
         Console.WriteLine("Палиндром");
+    }
+    else
+    {
+    Console.WriteLine("Не палиндром");
     }
 }
 else
